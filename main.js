29b5383b6d0b5913994883fca4422fa54362b5b5
Kreisi-Paul <111511210+Kreisi-Paul@users.noreply.github.com>
2024-10-20 22:54:52 +0200

    Updated Electron

--- main.js
+++ main.js
@@ -11,10 +11,7 @@ let jobInfo;
 
 
 let localDB = JSON.parse(fs.readFileSync(path.resolve("db/storage.json")));
-
-
 let clientVersion = fs.readFileSync(path.join(__dirname, "version.txt")).toString();
-
 
 
 app.whenReady().then(() => {
